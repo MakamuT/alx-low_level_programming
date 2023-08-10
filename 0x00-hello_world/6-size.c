a5e24b807175a7ae95a2ba91a4902e4397a10888
Thembisile <makamuthe2@gmail.com>
2023-08-10 16:04:09 -0700

    Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,17 @@
 /**
  * main - main function
  *
- * Return : must be 0
+ * Return: 0
+ *
  */
+
 
 int main(void)
 {
-	printf("gcc 6-size.c -m32: %lu byte(s) \n", sizeof(-m32));
-	printf("gcc 6-size.c -m64: %lu byte(s) \n", size(-m64));
-	printf("Size of a char: %lu byte(s) \n", sizeof(char));
-	printf("Size of an int: %lu byte(s) \n", sizeof(int));
-	printf("Size of a long int: %lu byte(s) \n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s) \n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s) \n", sizeof(float));
+	printf("size of char: %lu byte(s) \n", sizeof(char));
+	printf("size of an int: %lu byte(s) \n", sizeof(int));
+	printf("size of a long int: %lu byte(s) \n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s) \n", sizeof(long long int));
+	printf("size of a float: %lu byte(s) \n", sizeof(float));
 	return (0);
 }
