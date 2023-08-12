00ff42aab366c11d72d13abf2345c6b57468799b
Thembisile <makamuthe2@gmail.com>
2023-08-12 13:52:16 -0700

    Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = '0';
+	while
+		(d < 0) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
