f4bee0660aa3d0207091647f6c7be0c90f8ff9e2
Thembisile <makamuthe2@gmail.com>
2023-08-15 05:45:50 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print in lowercase
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter = 'z' ; letter++)
+		_putchar('letter');
+	putchar('\n');
+	return (0);
+}
