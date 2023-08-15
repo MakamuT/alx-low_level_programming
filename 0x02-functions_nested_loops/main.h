94c873a0c4d9a9edcc4a3c3ea0731ec4872e611b
Thembisile <makamuthe2@gmail.com>
2023-08-15 11:01:00 -0700

    last digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int i);
-int print_last_digit(int);
+int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
