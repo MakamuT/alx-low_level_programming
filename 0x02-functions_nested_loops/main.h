589019f061bcf172a55ab61fcaf5a77faf1302e2
Thembisile <makamuthe2@gmail.com>
2023-08-15 10:35:25 -0700

    abs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,12 @@
-#ifndef MAIN_h
-#define MAIN_h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int);
+int _abs(int i);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
