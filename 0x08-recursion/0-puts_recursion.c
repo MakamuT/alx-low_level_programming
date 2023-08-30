80c36a47db7312bc5749b80d75fb693ac1d2bec1
Thembisile <makamuthe2@gmail.com>
2023-08-30 11:28:35 -0700

    strings

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ * @s: input value
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
