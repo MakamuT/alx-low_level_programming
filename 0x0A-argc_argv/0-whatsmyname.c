ae8a1d9b5f906ec50d8dfb6c820a8e75f0dfaed4
Thembisile <makamuthe2@gmail.com>
2023-09-01 17:49:27 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,12 @@
  * @argv: char
  * Return: 0
  */
-int main(int argc, char *argv[])
-{
-	(void)argc;
 
-	printf(argv[0], "%s\n");
+int main(int argc, char *argv[])
+
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
 	return (0);
 }
