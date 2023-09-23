73b0d304d82f054ae2c1fa3bf3c8898de0378eee
Thembisile <makamuthe2@gmail.com>
2023-09-23 16:18:32 -0700

    holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .data
-	hello_msg db "Hello, Holberton",0x0A,0
+	hello_msg db "Hello, Holberton",0x0A,0 ;
 
 section .text
 	global main
 	extern printf
 main:
-sub rsp, 8
-mov rdi, hello_msg
-call printf,
-add rsi, 8
-mov rax, 60
-xor rdi, rdi
+sub rsp, 8 ;
+mov rdi, hello_msg ;
+call printf ;
+add rsi, 8 ;
+mov rax, 60 ;
+xor rdi, rdi ;
 syscall
 
 section .bss
