d0dfbbdf17bbc8ef8b483f4a607dfb646959f995
Thembisile <makamuthe2@gmail.com>
2023-09-23 15:36:36 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_first - prints a string before  the main function is executed
+ *
+ * Return: void
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
