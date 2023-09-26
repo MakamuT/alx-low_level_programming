6ecf150ecc46747ae665291972828a5bf12d0356
Thembisile <makamuthe2@gmail.com>
2023-09-26 05:18:07 -0700

    add new node

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -23,6 +24,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 #endif
