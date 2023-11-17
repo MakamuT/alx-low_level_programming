ad99e481698d6703bb1fe4347f8e39e27a9980e6
MakamuT <makamuthe2@gmail.com>
2023-11-17 13:09:38 -0800

    static

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower  - _islower
+ *@c: parameter to be checked
+ * Return: 1 and 0
+ */
+
+int _islower(int c)
+{
+	int _islower(int c);
+	{
+		if (c >= 'a' && c <= 'z')
+			return (1);
+		else
+			return (0);
+	}
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - function to find the absolute value of a number
+ *@i: function parameter
+ *Return: -i or i
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+		return (-i);
+	else
+		return (i);
+}
