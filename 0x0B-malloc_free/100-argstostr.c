81115aa74f15c8333b4a2f6af402e71586a81110
MakamuT <makamuthe2@gmail.com>
2023-11-21 14:12:40 -0800

    args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * *argstostr - oncatenates all the arguments
+ * @ac: int
+ * @av: char
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, arg2, n;
+	int size = ac;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (arg2 = 0; av[arg][arg2]; arg2++)
+			size++;
+	}
+	str = malloc(sizeof(char) * size + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	n = 0;
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (arg2 = 0; av[arg][arg2]; arg2++)
+		{
+			str[n++] = av[arg][arg2];
+		}
+	}
+	str[n++] = '\n';
+	return (str);
+}
+
