62caeb2451fdb3067997746db8127ae2d64916c4
MakamuT <makamuthe2@gmail.com>
2023-11-21 15:38:37 -0800

    args

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,6 +11,9 @@ char *argstostr(int ac, char **av)
 	int arg, arg2, n;
 	int size = ac;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
 	for (arg = 0; arg < ac; arg++)
 	{
 		for (arg2 = 0; av[arg][arg2]; arg2++)
