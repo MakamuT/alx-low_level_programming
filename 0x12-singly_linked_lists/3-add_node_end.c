2b0d52c9e26eac876d611ace4d641bce9ea22b41
MakamuT <makamuthe2@gmail.com>
2023-12-09 00:51:02 -0800

    node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_node_end - adds a new node at the end of a list_t
+ * @head: the first node
+ * @str: const
+ * Return: NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *node;
+	size_t n;
+
+	new = malloc(sizeof(list_t));
+
+	for (n = 0; str[n]; n++)
+		;
+	new->len = n;
+	new->next = *head;
+	node = *head;
+
+	if (node == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (node->next != NULL)
+			node = node->next;
+		node->next = new;
+	}
+	return (*head);
+}
