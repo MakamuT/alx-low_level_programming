752593d8654b892eda8f60f9d4d672a068a47d3d
MakamuT <makamuthe2@gmail.com>
2023-12-09 19:31:21 -0800

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * first - entry point
+ * Return: void
+ */
+void __attribute__ ((constructor)) first()
+{
+	printf("You are beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
