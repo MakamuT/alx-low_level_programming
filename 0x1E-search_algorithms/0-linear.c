577e416b63da883326b20d7813009b2155ec372a
MakamuT <makamuthe2@gmail.com>
2024-05-20 12:48:16 -0700

    search algo

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for a value using the Linear search algorithm
+ * @array: pointer to the first element
+ * @size: num of elements in an array
+ * @value: value to be searched
+ * Return: NULL or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t t;
+
+	if (array == NULL)
+		return (-1);
+
+	for (t = 0; t < size; t++)
+	{
+		printf("Value checed array[%ld] = [%d]\n", t, array[t]);
+		if (array[t] == value)
+			return (t);
+	}
+	return (-1);
+}
