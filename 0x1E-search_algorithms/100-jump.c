1dc98b552037c0e55609991526a5116a2327b3c3
MakamuT <makamuthe2@gmail.com>
2024-05-22 02:50:56 -0700

    search algo

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+/**
+ * jump_search - searches for a value using jump search algorithm
+ * @value:  value to search for
+ * @size: num of elements
+ * @array: pointer
+ * Return: NULL or -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t t, jump, step;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+	for (t = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("value checked array[%ld] = [%d]\n", jump, array[jump]);
+		t = jump;
+		jump += step;
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", t, jump);
+
+	jump = jump < size - 1 ? jump : size - 1;
+	for (; t < jump && array[t] < value; t++)
+		printf("Value checked array [%ld] = [%d]\n", t, array[t]);
+	printf("Value checked array [%ld] = [%d]\n", t, array[t]);
+
+	return (array[t] == value ? (int)t : -1);
+}
